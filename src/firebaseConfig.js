2359c181a6a25f52dfbc3173a10256535c8659b3
Nader Zaman <naderzaman50@gmail.com>
2023-12-29 10:57:46 +0500

    FirebaseConfig.js
    Added firebaseConfig.js file in the project

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAkkns5pWoRJCyGLHNPBK9CtRjJqV5oVm0",
+  authDomain: "fir-tutorial-6ea72.firebaseapp.com",
+  projectId: "fir-tutorial-6ea72",
+  storageBucket: "fir-tutorial-6ea72.appspot.com",
+  messagingSenderId: "869392763957",
+  appId: "1:869392763957:web:ac50f57801891d6443757c"
+};
+const app = initializeApp(firebaseConfig);
